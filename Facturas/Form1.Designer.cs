58859fbf333b67c6eb6a4faea809ed1c23e4835a
sistemas3refriauto <sistemas3@refriauto.com.mx>
2018-10-22 08:28:01 -0700

    Se quitaron algunos archivos extras que no comprometian con la funcionalidad del programa en si

--- Facturas/Form1.Designer.cs
+++ Facturas/Form1.Designer.cs
@@ -115,7 +115,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Generador - v 5.0.0.4";
+            this.Text = "Generador - v 5.0.0.5";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
